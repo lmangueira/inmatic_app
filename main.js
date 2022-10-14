e926a2b035f5ca89186bddd66a24ac7eac702693
Manuel Angueira <manuel.angueira@coneptum.com>
2022-10-14 12:32:43 +0200

    Set icon app for both installation and executable. Updated min res

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const { app, BrowserWindow } = require('electron')
 
 function createMainWindow() {
+    const __assets_dir = './assets'
     const mainWindow = new BrowserWindow({
         title: 'Inmatic.ai',
-        minWidth: 1000,
-        minHeight: 800,
+        minWidth: 1280,
+        minHeight: 1024,
+        icon: __assets_dir + '/icon/icon.png'
     })
+    
     mainWindow.removeMenu()
-    mainWindow.loadURL('https://app.inmatic.ai')
+    mainWindow.loadURL('https://app.inmatic.ai', {
+        userAgent: "InmaticApp"
+    })
 }
 
 app.on('ready', createMainWindow)
