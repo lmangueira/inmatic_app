bed9cb6e3e62b40ba37aa9bbda00677e37209ade
Manuel Angueira <luis.manuel.angueira@gmail.com>
2022-10-15 00:55:47 +0200

    Improved UX. Splash screen while app not ready

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
 
 function createMainWindow() {
     const __assets_dir = './assets'
+
     const mainWindow = new BrowserWindow({
         title: 'Inmatic.ai',
-        minWidth: 1280,
-        minHeight: 1024,
-        icon: __assets_dir + '/icon/icon.png'
-    })
-    
+        width: 1280,
+        height: 1024,
+        minWidth: 1024,
+        minHeight: 768,
+        show: false,
+        icon: __assets_dir + '/icon/icon.png',
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+          }
+    });
+
     mainWindow.removeMenu()
     mainWindow.loadURL('https://app.inmatic.ai', {
         userAgent: "InmaticApp"
     })
+
+    const splashWindow = new BrowserWindow({ 
+        width: 500, 
+        height: 300, 
+        transparent: true, 
+        frame: false, 
+        alwaysOnTop: true 
+      });
+
+    splashWindow.loadFile('splash.html')
+    splashWindow.center()
+
+    mainWindow.once('ready-to-show', () => {
+        splashWindow.close()
+
+        mainWindow.show()
+        mainWindow.maximize()
+    })
+
 }
 
 app.on('ready', createMainWindow)
